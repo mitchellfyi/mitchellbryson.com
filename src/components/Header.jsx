cac551155394175231fa123141201a8b85f99b1e
Mitchell Bryson <198377+mitchellfyi@users.noreply.github.com>
2025-09-08 14:11:50 +0100

    comment out unused pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -116,8 +116,8 @@ function MobileNavigation(props) {
             <MobileNavItem href="/about">About</MobileNavItem>
             <MobileNavItem href="/articles">Articles</MobileNavItem>
             <MobileNavItem href="/projects">Projects</MobileNavItem>
-            <MobileNavItem href="/speaking">Speaking</MobileNavItem>
-            <MobileNavItem href="/uses">Uses</MobileNavItem>
+            {/* <MobileNavItem href="/speaking">Speaking</MobileNavItem>
+            <MobileNavItem href="/uses">Uses</MobileNavItem> */}
             <MobileNavItem href="/contact">Contact</MobileNavItem>
           </ul>
         </nav>
@@ -156,8 +156,8 @@ function DesktopNavigation(props) {
         <NavItem href="/about">About</NavItem>
         <NavItem href="/articles">Articles</NavItem>
         <NavItem href="/projects">Projects</NavItem>
-        <NavItem href="/speaking">Speaking</NavItem>
-        <NavItem href="/uses">Uses</NavItem>
+        {/* <NavItem href="/speaking">Speaking</NavItem>
+        <NavItem href="/uses">Uses</NavItem> */}
         <NavItem href="/contact">Contact</NavItem>
       </ul>
     </nav>
